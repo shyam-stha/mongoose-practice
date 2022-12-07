0652e0b040a37dadef15dfdf6a5067a486603a94
Shyam Stha <sthask2019@gmail.com>
2022-12-07 21:28:38 +0545

    Add mongoose and create schema , model and methods

---
+++ index.js
@@ -0,0 +1,38 @@
+import mongoose, { Schema, model } from "mongoose";
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/test')    //making connection with mongodb test database 
+        console.log("Database Connected Sucessfully...")
+
+        //define schema for Human which will have name feild of type string
+        const HumanSchema = new Schema({
+            name: String
+        })
+
+        //adds greeting method to the HumanSchema 
+        HumanSchema.methods.greeting = function greeting() {
+            /* functions added to the methods property of a schema get compiled into the Model prototype and exposed on each document instance: */
+            const greeting = this.name ? "Good morning, it's me " + this.name : "I don't have name";
+            console.log(greeting)
+        }
+
+        //create new model, its nothing but a class with which we will use to construct documents
+        const Human = new model('Human', HumanSchema)
+
+        //defining a document with field property of name : "shyam"
+        const shyam = new Human({ name: "shyam" })
+        
+        //similarly, defining a document with field property of name : nisha
+        const nisha = new Human({ name: "nisha" })
+
+        //invoking function added to the methods property of schema
+        nisha.greeting()
+
+
+    } catch (error) {
+        console.log("error", error)
+    }
+}
+
+connectDB();

092e84e80ffb0cbbbe12730d313f1a2b30e57de8
Shyam Stha <sthask2019@gmail.com>
2022-12-07 22:32:47 +0545

    Create bookSchema and model, add instance method to the bookSchema

---
+++ practice.js
@@ -0,0 +1,65 @@
+import mongoose, { model, Schema } from "mongoose";
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/test')
+        console.log("Database connected sucessfully...")
+    } catch (error) {
+        console.log("Error ocured during db connection: ", error)
+    }
+}
+
+connectDB();
+
+//defining mongoose schema for book 
+const bookSchema = new Schema({
+    name: String,
+    pages: Number,
+    author: String,
+    publisher: String
+})
+
+//schema.add method will add additonal keys 
+// bookSchema.add({edition : String})
+
+//adding instance method to the bookSchema
+bookSchema.methods.findSamePublisher = function (cb) {
+    return new model('Book').find({ publisher: this.publisher }, cb)
+}
+
+//creating a model of using bookSchema 
+const Book = new model('Book', bookSchema);
+
+const munamadan = new Book({
+    name: "munamadan",
+    pages: 270,
+    author: "Laxmi  shrestha",
+    publisher: "samar nepal"
+})
+
+const jiwankadakiful = new Book({
+    name: "jiwankadakiful",
+    pages: 199,
+    author: "jhamak kumari ghimire",
+    publisher: "samar nepal"
+})
+
+//saving new document using Book model
+// await jiwankadakiful.save()
+// await munamadan.save()
+
+
+//loging munamadan book 
+// console.log(munamadan)
+
+//invoking intance method findSamePublisher which wiil give us a list of books with same publisher
+munamadan.findSamePublisher((err, publishers) => {
+        console.log(publishers)
+})
+
+
+
+
+
+
+
